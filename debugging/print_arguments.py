f587640fcde7fb7bd9abd90e15f9c5391798752e
Ibrux11 <ibraluunge@gmail.com>
2024-09-02 14:06:43 +0200

    yes my friend

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1  
-    return result
-
 if len(sys.argv) > 1:
-    for arg in sys.argv[1:]:
-        f = factorial(int(arg))
-        print(f)
+    for i in range(1, len(sys.argv)):
+        print(sys.argv[i])
+else:
+    print("No arguments provided.")
